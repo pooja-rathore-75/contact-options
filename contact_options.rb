1d4fb3167a51b3cab5a262ade4b954e0bd1a9440
pooja <poojarathore7598@gmail.com>
2023-07-06 12:31:49 +0530

    Calculate ranking.

--- contact_options.rb
+++ contact_options.rb
@@ -4,4 +4,14 @@ file_path = 'contact_options.json'
 json_data = File.read(file_path)
 
 contacts = JSON.parse(json_data)
-puts contacts
+
+contacts.each do |contact|
+  ranking = 3
+  ranking += 2 unless ['gmail.com', 'hotmail.com', 'outlook.com'].include? contact["email"] 
+
+  offer = contact["introsOffered"].values.sum
+  ranking += offer
+
+  contact["ranking"] = ranking
+  puts "****************#{contact}****************"
+end
